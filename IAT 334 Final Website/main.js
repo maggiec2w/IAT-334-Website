b26889e16976e93c1ee68c96535ec22aacfe4278
Maggie <magchanyw@gmail.com>
2019-07-29 00:08:34 -0700

    added all section

---
+++ IAT 334 Final Website/main.js
@@ -0,0 +1,35 @@
+// window.onscroll = function() {myFunction()};
+
+// var navbar = document.getElementById("main-nav");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky")
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
+
+var pic1 = document.getElementsByClassName("type p");
+
+function doStuff(){
+    
+    // var pic2 = document.getElementById(".type h1");
+    // var pic3 = document.getElementById(".type h3");
+
+    pic1.style.webkitAnimationPlayState="paused";
+    // pic2.style.webkitAnimationPlayState="paused";
+    // pic3.style.webkitAnimationPlayState="paused";
+}
+
+// $(window).on('scroll', function() {
+//         var pic1 = document.getElementById(".type p");
+//     var pic2 = document.getElementById(".type h1");
+//     var pic3 = document.getElementById(".type h3");
+
+//     pic1.style.webkitAnimationPlayState="paused";
+//     pic2.style.webkitAnimationPlayState="paused";
+//     pic3.style.webkitAnimationPlayState="paused";
+// });
+
